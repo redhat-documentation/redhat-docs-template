81ace8e64245aca5889e81fe62e32884df066811
aireilly <aireilly@redhat.com>
2023-06-01 16:12:24 +0100

    tweak Containerfile

--- build.sh
+++ build.sh
@@ -6,15 +6,16 @@ ASSEMBLY="$1"
 
 asciidoctor "$ASSEMBLY" -o index.html \
 -a stylesheet="assets/css/redhat.css" \
+-a logo="assets/img/logo.svg" \
 -a toc -a toc-placement=left \
 -a docinfodir="assets" \
 -a docinfo=shared \
 -a source-highlighter=rouge \
 -a rouge-style=github \
--a favicon="favicon.ico" \
+-a favicon="assets/img/favicon.ico" \
 -a icon-set=fab \
 -a icons=font \
 -a sectlinks 
 
 echo ""
-echo "finished building "$ASSEMBLY" as index.html"
+echo "Built $ASSEMBLY as ./index.html"
